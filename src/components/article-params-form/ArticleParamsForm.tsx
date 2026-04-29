4787260a7efa8072272f03fe239b958243e86913
Viktor <BrutalName@yandex.by>
2026-04-29 23:10:14 +0300

    Проектная 10

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,14 +1,73 @@
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
+import { Text } from 'src/ui/text';
+
+import { useState, useRef, useEffect, ReactNode, SyntheticEvent } from 'react';
 
 import styles from './ArticleParamsForm.module.scss';
 
-export const ArticleParamsForm = () => {
+type FormProps = {
+    children: ReactNode
+	onReset: () => void
+	onSubmit:() => void
+}
+
+export const ArticleParamsForm = (props: FormProps) => {
+
+	const [state, setState] = useState(false);
+	const formRef = useRef<HTMLElement | null>(null)
+
+	const clickFormButton = () => {
+		setState(!state);
+		if (formRef.current) {
+			formRef.current.classList.toggle(styles.container_open);
+		}
+	}
+
+	const handleClickOutside = (event: MouseEvent) => {
+		if (formRef.current && !formRef.current.contains(event.target as Node)) {
+			setState(false);
+			if (formRef.current.classList.contains(styles.container_open)) {
+				formRef.current.classList.remove(styles.container_open);
+			}
+		}
+	};
+
+	useEffect(() => {
+		if (state) {
+			document.addEventListener('mousedown', handleClickOutside);
+		} else {
+			document.removeEventListener('mousedown', handleClickOutside);
+		}
+		return () => {
+			document.removeEventListener('mousedown', handleClickOutside);
+		};
+	}, [state]);
+
+	const handleSubmit = (event: SyntheticEvent) => {
+		event.preventDefault()
+		if(props.onSubmit) {
+			props.onSubmit()
+		}
+	}
+
+	const handleReset = () => {
+		if(props.onReset) {
+			props.onReset()
+		}
+	}
+
 	return (
 		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
-				<form className={styles.form}>
+			<ArrowButton isOpen={state} onClick={clickFormButton} />
+			<aside ref={formRef} className={styles.container}>
+				<form className={styles.form} onSubmit={handleSubmit} onReset={handleReset}>
+					<div className={styles.form_childrens}>
+						<Text size={31} weight={800} uppercase>
+							Задайте параметры			
+						</Text>
+						{props.children}
+					</div>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
 						<Button title='Применить' htmlType='submit' type='apply' />
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,15 @@
 import { createRoot } from 'react-dom/client';
-import { StrictMode, CSSProperties } from 'react';
+import { StrictMode, CSSProperties, useState, useLayoutEffect, useRef } from 'react';
 import clsx from 'clsx';
 
 import { Article } from './components/article/Article';
 import { ArticleParamsForm } from './components/article-params-form/ArticleParamsForm';
-import { defaultArticleState } from './constants/articleProps';
+import { defaultArticleState, fontFamilyOptions, fontSizeOptions, fontColors, backgroundColors, contentWidthArr, ArticleStyle } from './constants/articleProps';
+
+import { Select } from './ui/select/Select'
+import { RadioGroup } from './ui/radio-group/RadioGroup'
+import { Separator } from './ui/separator/Separator'
+
 
 import './styles/index.scss';
 import styles from './styles/index.module.scss';
@@ -13,19 +18,103 @@ const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
 
 const App = () => {
+
+	const [articleFont, setArticleFont] = useState(defaultArticleState.fontFamilyOption);
+	const [articleSize, setArticleSize] = useState(defaultArticleState.fontSizeOption);
+	const [articleColor, setArticleColor] = useState(defaultArticleState.fontColor);
+	const [articleContentWidth, setArticleContentWidth] = useState(defaultArticleState.contentWidth);
+	const [articleBackgroundColors, setArticleBackgroundColors] = useState(defaultArticleState.backgroundColor);
+
+	const mainRef = useRef<HTMLElement | null>(null);
+
+	const currentArticleStyle: ArticleStyle = {
+		fontFamilyOption: articleFont,
+		fontSizeOption: articleSize,
+		fontColor: articleColor,
+		contentWidth: articleContentWidth,
+		backgroundColor: articleBackgroundColors,
+	}
+
+	const resetStyleForm = () => {
+		setArticleFont(defaultArticleState.fontFamilyOption)
+		setArticleSize(defaultArticleState.fontSizeOption)
+		setArticleColor(defaultArticleState.fontColor)
+		setArticleBackgroundColors(defaultArticleState.backgroundColor)
+		setArticleContentWidth(defaultArticleState.contentWidth)
+	}
+
+	const submitForm = (styleForm: ArticleStyle) => {
+		if (mainRef.current) {
+			const style = mainRef.current.style;
+			style.setProperty('--font-family', styleForm.fontFamilyOption.value);
+			style.setProperty('--font-size', styleForm.fontSizeOption.value);
+			style.setProperty('--font-color', styleForm.fontColor.value);
+			style.setProperty('--container-width', styleForm.contentWidth.value);
+			style.setProperty('--bg-color', styleForm.backgroundColor.value);
+		}
+	}
+
+	useLayoutEffect(() => {
+		submitForm(defaultArticleState)
+	}, [])
+
 	return (
 		<main
 			className={clsx(styles.main)}
-			style={
-				{
-					'--font-family': defaultArticleState.fontFamilyOption.value,
-					'--font-size': defaultArticleState.fontSizeOption.value,
-					'--font-color': defaultArticleState.fontColor.value,
-					'--container-width': defaultArticleState.contentWidth.value,
-					'--bg-color': defaultArticleState.backgroundColor.value,
-				} as CSSProperties
-			}>
-			<ArticleParamsForm />
+			ref={mainRef}
+			>
+			<ArticleParamsForm
+				onReset={() => {
+					submitForm(defaultArticleState)
+					resetStyleForm()
+				}}
+				onSubmit={() => {
+					submitForm(currentArticleStyle)
+				}}
+			>
+				<Select 
+					options={fontFamilyOptions} 
+					selected={articleFont} 
+					title={'шрифт'} 
+					onChange={(option) => {
+						setArticleFont(option)
+					}} 
+				/>
+				<RadioGroup
+					name={'fsize'}
+					options={fontSizeOptions}
+					selected={articleSize}
+					onChange={(value) => {
+						setArticleSize(value)
+					}} 
+					title={'размер шрифта'}
+				/>
+				<Select 
+					options={fontColors} 
+					selected={articleColor} 
+					title={'Цвет шрифта'} 
+					onChange={(option) => {
+						setArticleColor(option)
+					}} 
+				/>
+				<Separator />
+				<Select 
+					options={backgroundColors} 
+					selected={articleBackgroundColors} 
+					title={'Цвет фона'} 
+					onChange={(option) => {
+						setArticleBackgroundColors(option)
+					}} 
+				/>
+				<Select 
+					options={contentWidthArr} 
+					selected={articleContentWidth} 
+					title={'Цвет фона'} 
+					onChange={(option) => {
+						setArticleContentWidth(option)
+					}} 
+				/>
+			</ArticleParamsForm>
 			<Article />
 		</main>
 	);
